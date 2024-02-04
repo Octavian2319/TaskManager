63e78a86cfd05d685771f0f591352a5feaf839cd
Octavian2319 <62910495+Octavian2319@users.noreply.github.com>
2024-02-04 15:01:40 +0200

    Update WelcomeController.java

--- WelcomeController.java
+++ WelcomeController.java
@@ -1,9 +1,12 @@
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class WelcomeController {
 
@@ -20,6 +23,9 @@ public class WelcomeController {
     private Button RegisterButton;
 
     @FXML
+    private AnchorPane Scene1;
+
+    @FXML
     private Label WelcomeText;
 
     @FXML
@@ -28,8 +34,8 @@ public class WelcomeController {
     }
 
     @FXML
-    void RegisterButton(ActionEvent event) {
-
+    void RegisterButton(ActionEvent event) throws IOException {
+        new SwitchScene(Scene1, "RegisterScene.fxml");
     }
 
 }
