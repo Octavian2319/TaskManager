cd6fa0a58069f3b1d91f991156a37434ca9d4626
Octavian2319 <62910495+Octavian2319@users.noreply.github.com>
2024-02-24 12:50:49 +0200

    Create AddController.java

---
+++ AddController.java
@@ -0,0 +1,27 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class AddController {
+
+    @FXML
+    private AnchorPane AddScene;
+
+    @FXML
+    private DatePicker Date;
+
+    @FXML
+    private TextArea TextArea;
+
+    @FXML
+    private TextField TitleField;
+
+    @FXML
+    void SubmitButton(ActionEvent event) {
+        
+    }
+
+}
