2e1c3e9316f3493e3c8ff5b7e7a398ffb542ad6a
Octavian2319 <62910495+Octavian2319@users.noreply.github.com>
2024-02-26 23:52:13 +0200

    Update WelcomeController.java

--- WelcomeController.java
+++ WelcomeController.java
@@ -1,7 +1,11 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -27,10 +31,26 @@ public class WelcomeController {
 
     @FXML
     private Label WelcomeText;
+    @FXML
+    private Parent root;
 
     @FXML
-    void LoginButton(ActionEvent event) {
+    void LoginButton(ActionEvent event) throws IOException {
+        String username = NameField.getText();
+        String password = PasswordField.getText();
+    
+        if (checkDetails(username, password)) {
+            System.out.println("Reusit");
+            DataStore.getInstance().setUsername(username);
+            System.out.println("Numele trimis este "+NameField.getText());
 
+            new SwitchScene(Scene1, "MainPage.fxml");
+        }
+    
+        else{
+            System.out.println("Nereusit");
+        }
+        
     }
 
     @FXML
@@ -38,4 +58,27 @@ public class WelcomeController {
         new SwitchScene(Scene1, "RegisterScene.fxml");
     }
 
+    private boolean checkDetails(String Name,String Password){
+        try{
+            File file=new File("details.txt");
+            FileReader fileReader=new FileReader(file);
+            BufferedReader bufferedReader=new BufferedReader(fileReader);
+            String line;
+
+            while((line=bufferedReader.readLine()) !=null){
+                String[] parts=line.split(",");
+                String storedUsername = parts[0];
+                String storePassword = parts[1];
+
+                if(storedUsername.equals(Name) && storePassword.equals(Password)){
+                    bufferedReader.close();
+                    return true;
+                }
+            }
+            bufferedReader.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
